d9b3f29a613a49a9c299818230c2aded9b4e88bb
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-13 08:35:02 +0530

    added: State census csv file

--- Day29StateCensus/Program.cs
+++ Day29StateCensus/Program.cs
@@ -4,9 +4,12 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the master branch for assignment : Day 29 State Census");
+            //Console.WriteLine("Welcome to the master branch for assignment : Day 29 State Census");
+            string filePath = "C:\\Users\\princ\\OneDrive\\Desktop\\dotnet\\Day29StateCensus\\Day29StateCensus\\Book1.csv";
+            StateCensusAnalyser analyser = new StateCensusAnalyser(filePath);
+            int numberOfRecords = analyser.LoadDataFromCSV();
+            Console.WriteLine("Number of records in the file: " + numberOfRecords);
 
-            
         }
     }
 }
