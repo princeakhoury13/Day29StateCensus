4be234598326f2614e34b8985b19e9bfc6a2586a
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-13 08:51:11 +0530

    added: test case to check for exception if wrong file is selected

--- StateCensusAnalyserTests/UnitTest1.cs
+++ StateCensusAnalyserTests/UnitTest1.cs
@@ -19,5 +19,19 @@ namespace StateCensusAnalyserTests
             // Assert
             Assert.AreEqual(expectedNumberOfRecords, actualNumberOfRecords);
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void LoadDataFromCSV_ShouldThrowException_WhenIncorrectFileIsProvided()
+        {
+            // Arrange
+            string filePath = "C:\\Users\\princ\\OneDrive\\Desktop\\dotnet\\Day29StateCensus\\Day29StateCensus\\IncorrectFile.csv";
+            StateCensusAnalyser analyser = new StateCensusAnalyser(filePath);
+
+            // Act and Assert
+            analyser.LoadDataFromCSV();
+        }
+
+
     }
 }
