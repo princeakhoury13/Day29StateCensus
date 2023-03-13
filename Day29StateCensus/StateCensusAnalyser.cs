a5a063ba4496c9112cca060fb50ac84c2ff4c2bb
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-13 09:05:14 +0530

    updated: ReadFromCSV now takes delimiter as parameter

--- Day29StateCensus/StateCensusAnalyser.cs
+++ Day29StateCensus/StateCensusAnalyser.cs
@@ -39,5 +39,37 @@ namespace Day29StateCensus
 
             return numberOfRecords;
         }
+
+        public int LoadDataFromCSV(char delimiter)
+        {
+            int numberOfRecords = 0;
+            try
+            {
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] fields = line.Split(delimiter);
+
+                        // Check that the number of fields is correct
+                        if (fields.Length != 5)
+                        {
+                            throw new Exception("Incorrect number of fields in row: " + line);
+                        }
+
+                        // Process the fields here
+                        numberOfRecords++;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                throw;
+            }
+
+            return numberOfRecords;
+        }
     }
 }
