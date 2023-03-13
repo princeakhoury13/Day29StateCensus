325563c8cc316eb0f17ed803883ea6db01234b18
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-13 08:33:53 +0530

    added: Master branch greeting message

---
+++ Day29StateCensus/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Day29StateCensus
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the master branch for assignment : Day 29 State Census");
+
+            
+        }
+    }
+}
